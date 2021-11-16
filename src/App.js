bf8fe267ee67f53f5e0fec3f339f02fdc7e6b938
zaidkh10 <zaidmarawan850@gmail.com>
2021-11-16 21:03:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import react from "react";
+import { Component } from "react";
+
+class App extends Component{
+  constructor(){
+  super()
+  this.state = {count : 0}
+  this.addclick= this.addclick.bind(this)
+  this.decreaseclick= this.decreaseclick.bind(this)
+  this.clearclick= this.clearclick.bind(this)
+
+
+}
+
+addclick(){this.setState(prevState => {
+return{count: prevState.count +1 }
+ })}
+
+clearclick(){this.setState(prevState => {
+return{count: prevState.count = 0 }
+})}
+
+decreaseclick(){
+  
+  this.setState(prevState => {
+
+return{
+
+  count: prevState.count -1 
+}
+
+  })
+}
+render(){
+  return(
+  <div >
+
+    <h1>  {this.state.count}</h1>
+    <button id="button" onClick = {this.addclick}> add it!</button>
+    <button onClick = {this.clearclick}> clear it!</button>
+    <button onClick = {this.decreaseclick}> decrease it!</button>
+
+
+  </div>
+  )
+}}
+export default App
